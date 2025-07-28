a13542f7da3b259f246a1c6a284eddf83bc1a72e
Onkar Bandichhode <onkarbandichhode@gmail.com>
2025-07-28 22:32:10 +0530

    Phase 1 Completed

---
+++ models/listing.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const ListingSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://unsplash.com/photos/a-living-room-filled-with-furniture-and-a-flat-screen-tv-JhkY1M6HN9Y",
+        set: (v) => v === "" ? "https://unsplash.com/photos/a-living-room-filled-with-furniture-and-a-flat-screen-tv-JhkY1M6HN9Y" : v
+    },
+    price: Number,
+    location: String,
+    country: String
+})
+
+const Listing = mongoose.model("Listing", ListingSchema)
+module.exports = Listing
